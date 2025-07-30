1263b6a67ce214496b9e7a0b7b9bd38b049df999
iraleigh-anderson <iraleigh.anderson@gov.bc.ca>
2025-07-30 12:49:06 -0700

    Dymanic filenames

--- Element_Record_SQL_Insert_Automation_20250709.R
+++ Element_Record_SQL_Insert_Automation_20250709.R
@@ -1,5 +1,4 @@
 library(dplyr)
-#####MUST INSERT A SYNONYM_SUBNATIONAL TABLE TO INCLUDE ALT SCI NAMES#######
 ###########################################
 #### PART 1 Ecosystem Element Creation ####
 ###########################################
@@ -17,61 +16,51 @@ example <- data.frame(
   lmh52= c(0,1,0)
 )
 
-# 1. INSERT Statements for SCIENTIFIC_NAME
-# this generates scientific name record for the CDC name for the element
-# CDC names may change over time.
+# 1. CDC Scientific Name
 example$sci.name.cdc.sql <- paste0(
-  "INSERT INTO scientific_name (SCIENTIFIC_NAME_ID, SCIENTIFIC_NAME, FORMATTED_SCIENTIFIC_NAME, D_NAME_CATEGORY_ID, D_CLASSIFICATION_LEVEL_ID, AUTHOR_NAME, REC_CREATE_USER) 
-  VALUES (",
-  "getnextseq('SCIENTIFIC_NAME_ID'), '",
+  "INSERT INTO scientific_name (SCIENTIFIC_NAME, FORMATTED_SCIENTIFIC_NAME, D_NAME_CATEGORY_ID, D_CLASSIFICATION_LEVEL_ID, AUTHOR_NAME, REC_CREATE_USER) 
+  VALUES ('",
   trimws(example$sci.cdc), "', '",
   trimws(example$sci.cdc.html), "', ",
   "11, 52, 'BC', 'ida lmh77');"
 )
 
-# 1. (Continued) INSERT Statements for SCIENTIFIC_NAME
-# this generates scientific name record for the BEC name for the element
-# BEC names should come verbatim from the vegetation hierarchy and should never be modified. They are the durable linkage between BEC and Biotics.
+# 1. BEC Scientific Name
 example$sci.name.bec.sql <- paste0(
-  "INSERT INTO scientific_name (SCIENTIFIC_NAME_ID, SCIENTIFIC_NAME, FORMATTED_SCIENTIFIC_NAME, D_NAME_CATEGORY_ID, D_CLASSIFICATION_LEVEL_ID, AUTHOR_NAME, REC_CREATE_USER) 
-  VALUES (",
-  "getnextseq('SCIENTIFIC_NAME_ID'), '",
+  "INSERT INTO scientific_name (SCIENTIFIC_NAME, FORMATTED_SCIENTIFIC_NAME, D_NAME_CATEGORY_ID, D_CLASSIFICATION_LEVEL_ID, AUTHOR_NAME, REC_CREATE_USER) 
+  VALUES ('",
   trimws(example$sci.bec), "', '",
   trimws(example$sci.bec.html), "', ",
   "11, 52, 'BC', 'ida lmh77');"
 )
 
-# 2. INSERT Statements for ELEMENT_GLOBAL
-# CONCEPT_REFERENCE_ID = 292448 is the draft reference for the 2026 veg hierarchy
+# 2. ELEMENT_GLOBAL
 example$elem.glo.sql <- paste0(
-  "INSERT INTO element_global (ELEMENT_GLOBAL_ID, ELCODE_BCD, GNAME_ID, CONCEPT_REFERENCE_ID, CONCEPT_NAME_ID, G_PRIMARY_COMMON_NAME, D_CLASSIFICATION_STATUS_ID, D_LANGUAGE_ID, REC_CREATE_USER) 
-  VALUES (",
-  "getnextseq('ELEMENT_GLOBAL_ID'), '",
+  "INSERT INTO element_global (ELCODE_BCD, GNAME_ID, CONCEPT_REFERENCE_ID, CONCEPT_NAME_ID, G_PRIMARY_COMMON_NAME, D_CLASSIFICATION_STATUS_ID, D_LANGUAGE_ID, REC_CREATE_USER) 
+  VALUES ('",
   trimws(example$elcode), "', ",
   "(SELECT SCIENTIFIC_NAME_ID FROM scientific_name WHERE SCIENTIFIC_NAME = '", 
-  trimws(example$sci.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",  "'292448', ",
+  trimws(example$sci.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
+  "'292448', ",
   "(SELECT SCIENTIFIC_NAME_ID FROM scientific_name WHERE SCIENTIFIC_NAME = '", 
   trimws(example$sci.bec), "' AND REC_CREATE_USER = 'ida lmh77'), ",
   "'", trimws(example$com.cdc), "', ",
   "1, 2, 'ida lmh77');"
 )
 
-
-# 3. INSERT Statements for SCIENTIFIC_NAME_REF
-# REFERENCE_ID = 292448 is the draft reference for the 2026 veg hierarchy
+# 3. SCIENTIFIC_NAME_REF
 example$sci.ref.sql <- paste0(
-  "INSERT INTO scientific_name_ref (SCIENTIFIC_NAME_ID, PRIMARY_NAME_REF_IND, REC_CREATE_USER, REFERENCE_ID, SCIENTIFIC_NAME_REF_ID) 
+  "INSERT INTO scientific_name_ref (SCIENTIFIC_NAME_ID, PRIMARY_NAME_REF_IND, REC_CREATE_USER, REFERENCE_ID) 
   VALUES (",
   "(SELECT scientific_name_id FROM scientific_name WHERE SCIENTIFIC_NAME = '", 
   trimws(example$sci.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
-  "'Y', 'ida lmh77', 292448, getnextseq('SCIENTIFIC_NAME_REF_ID'));"
+  "'Y', 'ida lmh77', 292448);"
 )
 
-# 4. INSERT Statements for ELEMENT_NATIONAL
+# 4. ELEMENT_NATIONAL
 example$elem.nat.sql <- paste0(
-  "INSERT INTO element_national (ELEMENT_NATIONAL_ID, ELEMENT_GLOBAL_ID, NATION_ID, NNAME_ID, N_PRIMARY_COMMON_NAME, D_LANGUAGE_ID, REC_CREATE_USER) 
+  "INSERT INTO element_national (ELEMENT_GLOBAL_ID, NATION_ID, NNAME_ID, N_PRIMARY_COMMON_NAME, D_LANGUAGE_ID, REC_CREATE_USER) 
   VALUES (",
-  "getnextseq('ELEMENT_NATIONAL_ID'), ",
   "(SELECT ELEMENT_GLOBAL_ID FROM element_global WHERE G_PRIMARY_COMMON_NAME = '", trimws(example$com.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
   "38, ",
   "(SELECT scientific_name_id FROM scientific_name WHERE SCIENTIFIC_NAME = '", trimws(example$sci.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
@@ -79,7 +68,7 @@ example$elem.nat.sql <- paste0(
   "1, 'ida lmh77');"
 )
 
-# 5. INSERT Statements for COMMUNITY_NATIONAL
+# 5. COMMUNITY_NATIONAL
 example$com.nat.sql <- paste0(
   "INSERT INTO community_national (ELEMENT_NATIONAL_ID, D_CURR_PRESENCE_ABSENCE_ID, D_DIST_CONFIDENCE_ID, REC_CREATE_USER) 
   VALUES (",
@@ -87,11 +76,10 @@ example$com.nat.sql <- paste0(
   "1, 1, 'ida lmh77');"
 )
 
-# 6. INSERT Statements for ELEMENT_SUBNATIONAL
+# 6. ELEMENT_SUBNATIONAL
 example$elem.sub.sql <- paste0(
-  "INSERT INTO element_subnational (ELEMENT_SUBNATIONAL_ID, ELEMENT_NATIONAL_ID, SUBNATION_ID, SNAME_ID, S_PRIMARY_COMMON_NAME, D_LANGUAGE_ID, REC_CREATE_USER, D_DATA_SENSITIVE_ID) 
+  "INSERT INTO element_subnational (ELEMENT_NATIONAL_ID, SUBNATION_ID, SNAME_ID, S_PRIMARY_COMMON_NAME, D_LANGUAGE_ID, REC_CREATE_USER, D_DATA_SENSITIVE_ID) 
   VALUES (",
-  "getnextseq('ELEMENT_SUBNATIONAL_ID'), ",
   "(SELECT ELEMENT_NATIONAL_ID FROM element_national WHERE N_PRIMARY_COMMON_NAME = '", trimws(example$com.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
   "61, ",
   "(SELECT scientific_name_id FROM scientific_name WHERE SCIENTIFIC_NAME = '", trimws(example$sci.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
@@ -99,7 +87,7 @@ example$elem.sub.sql <- paste0(
   "1, 'ida lmh77', 2);"
 )
 
-# 7. INSERT Statements for COMMUNITY_SUBNATIONAL
+# 7. COMMUNITY_SUBNATIONAL
 example$com.sub.sql <- paste0(
   "INSERT INTO community_subnational (ELEMENT_SUBNATIONAL_ID, D_CURR_PRESENCE_ABSENCE_ID, D_DIST_CONFIDENCE_ID, REC_CREATE_USER) 
   VALUES (",
@@ -108,39 +96,30 @@ example$com.sub.sql <- paste0(
   "1, 1, 'ida lmh77');"
 )
 
-# 8. INSERT Statements for ELEMENT_SUBNATL_REF
-# REFERENCE_ID = 292448 is the draft reference for the 2026 veg hierarchy
+# 8. ELEMENT_SUBNATL_REF - veg hierarchy
 example$sub.ref.vh.sql <- paste0(
-  "INSERT INTO element_subnatl_ref (ELEMENT_SUBNATL_REF_ID, ELEMENT_SUBNATIONAL_ID, REFERENCE_ID, SCIENTIFIC_NAME_IND, ",
-  "PRIMARY_SCIENTIFIC_NAME_IND, CLASSIFICATION_TAXONOMY_IND, RANK_FACTORS_IND, REC_CREATE_USER) 
+  "INSERT INTO element_subnatl_ref (ELEMENT_SUBNATIONAL_ID, REFERENCE_ID, SCIENTIFIC_NAME_IND, PRIMARY_SCIENTIFIC_NAME_IND, CLASSIFICATION_TAXONOMY_IND, RANK_FACTORS_IND, REC_CREATE_USER) 
   VALUES (",
-  "getnextseq('ELEMENT_SUBNATL_REF'), ",
   "(SELECT ELEMENT_SUBNATIONAL_ID FROM element_subnational WHERE S_PRIMARY_COMMON_NAME = '", 
   trimws(example$com.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
   "292448, 'Y', 'Y', 'Y', 'N', 'ida lmh77');"
 )
 
-# 8. (Continued) INSERT Statements for ELEMENT_SUBNATL_REF
-# REFERENCE_ID = 292449 is the draft reference for LMH77
+# 8. ELEMENT_SUBNATL_REF - lmh77
 example$sub.ref.77.sql <- paste0(
-  "INSERT INTO element_subnatl_ref (ELEMENT_SUBNATL_REF_ID, ELEMENT_SUBNATIONAL_ID, REFERENCE_ID, SCIENTIFIC_NAME_IND, ",
-  "PRIMARY_SCIENTIFIC_NAME_IND, CLASSIFICATION_TAXONOMY_IND, RANK_FACTORS_IND, REC_CREATE_USER) 
+  "INSERT INTO element_subnatl_ref (ELEMENT_SUBNATIONAL_ID, REFERENCE_ID, SCIENTIFIC_NAME_IND, PRIMARY_SCIENTIFIC_NAME_IND, CLASSIFICATION_TAXONOMY_IND, RANK_FACTORS_IND, REC_CREATE_USER) 
   VALUES (",
-  "getnextseq('ELEMENT_SUBNATL_REF'), ",
   "(SELECT ELEMENT_SUBNATIONAL_ID FROM element_subnational WHERE S_PRIMARY_COMMON_NAME = '", 
   trimws(example$com.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
   "292449, 'N', 'N', 'Y', 'Y', 'ida lmh77');"
 )
 
-# 8. (Continued) INSERT Statements for ELEMENT_SUBNATL_REF
-# REFERENCE_ID = 169749 is the reference for LMH 52
+# 8. ELEMENT_SUBNATL_REF - lmh52 (conditional)
 example$sub.ref.52.sql <- ifelse(
   example$lmh52 == 1,
   paste0(
-    "INSERT INTO element_subnatl_ref (ELEMENT_SUBNATL_REF_ID, ELEMENT_SUBNATIONAL_ID, REFERENCE_ID, SCIENTIFIC_NAME_IND, ",
-    "PRIMARY_SCIENTIFIC_NAME_IND, CLASSIFICATION_TAXONOMY_IND, RANK_FACTORS_IND, REC_CREATE_USER) 
+    "INSERT INTO element_subnatl_ref (ELEMENT_SUBNATIONAL_ID, REFERENCE_ID, SCIENTIFIC_NAME_IND, PRIMARY_SCIENTIFIC_NAME_IND, CLASSIFICATION_TAXONOMY_IND, RANK_FACTORS_IND, REC_CREATE_USER) 
     VALUES (",
-    "getnextseq('ELEMENT_SUBNATL_REF'), ",
     "(SELECT ELEMENT_SUBNATIONAL_ID FROM element_subnational WHERE S_PRIMARY_COMMON_NAME = '", 
     trimws(example$com.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
     "169749, 'N', 'N', 'N', 'Y', 'ida lmh77');"
@@ -148,21 +127,20 @@ example$sub.ref.52.sql <- ifelse(
   "[skipped]"
 )
 
-# 9. INSERT Statements for OTHER_SUB_COMMON_NAME
+# 9. OTHER_SUB_COMMON_NAME
 example$oth.sub.com.sql <- paste0(
-  "INSERT INTO other_sub_common_name (OTHER_SUB_COMMON_NAME_ID, ELEMENT_SUBNATIONAL_ID, OTHER_SUB_COMMON_NAME, D_LANGUAGE_ID, REC_CREATE_USER) 
+  "INSERT INTO other_sub_common_name (ELEMENT_SUBNATIONAL_ID, OTHER_SUB_COMMON_NAME, D_LANGUAGE_ID, REC_CREATE_USER) 
   VALUES (",
-  "getnextseq('OTHER_SUB_COMMON_NAME_ID'), ",
   "(SELECT ELEMENT_SUBNATIONAL_ID FROM element_subnational WHERE S_PRIMARY_COMMON_NAME = '", 
   trimws(example$com.cdc), "' AND REC_CREATE_USER = 'ida lmh77'), ",
   "'", trimws(example$com.bec), "', ",
   "1, 'ida lmh77');"
 )
 
-# 10. INSERT Statements for EL_SUBNATL_AGENCY_STATUS
+# 10. EL_SUBNATL_AGENCY_STATUS
 example$agen.stat.sql <- paste0(
-  "INSERT INTO el_subnatl_agency_status (EL_SUBNATL_AGENCY_STATUS_ID, ELEMENT_SUBNATIONAL_ID, AGENCY_NAME, AGENCY_STATUS, REC_CREATE_USER) 
-  VALUES (","getnextseq('EL_SUBNATL_AGENCY_STATUS_ID'), ",
+  "INSERT INTO el_subnatl_agency_status (ELEMENT_SUBNATIONAL_ID, AGENCY_NAME, AGENCY_STATUS, REC_CREATE_USER) 
+  VALUES (",
   "(SELECT ELEMENT_SUBNATIONAL_ID FROM element_subnational WHERE S_PRIMARY_COMMON_NAME = '",
   trimws(example$com.cdc), 
   "' AND REC_CREATE_USER = 'ida lmh77'), ",
@@ -170,6 +148,7 @@ example$agen.stat.sql <- paste0(
 )
 
 # 11. INSERT Statements for BC_COMM_SUB_ECOSYS_GRP 
+#getnextseq required here because this is an extensible table
 example$eco.grp.sql <- paste0(
   "INSERT INTO bc_comm_sub_ecosys_grp (BC_COMM_SUB_ECOSYS_GRP_ID, ELEMENT_SUBNATIONAL_ID, ECOSYS_GRP_ID, DATE_ASSESSED, REC_CREATE_USER) 
   VALUES (","getnextseq('BC_COMM_SUB_ECOSYS_GRP_ID'), ",
@@ -225,6 +204,7 @@ BGCexample <- data.frame(
 
 # 12. INSERT Statements for BC_COMM_SUB_BGC 
 #E26MAC01BCCA is the 2026 BEC vegetation hierarchy
+#getnextseq required here because this is an extensible table
 BGCexample$eco.grp.sql <- paste0(
   "INSERT INTO bc_comm_sub_bgc (BC_COMM_SUB_BGC_ID, ELEMENT_SUBNATIONAL_ID, BGC_CD, SITE_SERIES, D_OCCURRENCE_STATUS_ID, NOTE, REC_CREATE_USER) VALUES (",
   "getnextseq('BC_COMM_SUB_BGC_ID'), ",
